cbee8470d8773517e4377df9b595d3a8f56a4d73
Kathryn <ktanardy@gmail.com>
2024-02-02 16:47:07 -0800

    Added courses page

--- client/App.js
+++ client/App.js
@@ -1,14 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, View } from 'react-native';
 import Header from './components/Header'
 import CourseContent from './components/CourseContent';
 
 export default function App() {
   return (
+    <>
     <View style={styles.container}>
-      <Header/>
-      <CourseContent/>
+      <ImageBackground
+        style={styles.background}
+        source={require('./img/Untitled-2.png')
+        }>
+          <Header/>
+          <CourseContent/>
+      </ImageBackground>
     </View>
+    <View>
+    
+    </View>
+    </>
   );
 }
 
@@ -16,5 +26,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    padding: 0,
   },
+  background: {
+    flex: 1,
+  }
 });
