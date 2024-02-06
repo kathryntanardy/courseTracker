08292c5321851c3b9f05e1fb41e0bddf4528a909
SamiBenAmara <samibenamara23@gmail.com>
2024-02-06 08:44:22 -0800

    Add CRUD operations for courses and items

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const Item = require('../models/models');
 const Course = require('../models/models');
 
 router.get('/', async (req, res) => {
@@ -20,15 +19,40 @@ router.post('/', async (req, res) => {
     const newCourse = new Course({
 
         name: req.body.name,
+        items: req.body.items,
         credits: req.body.credits
 
     });
 
     try {
         await newCourse.save();
-        res.status(201).json({ message: `Created course ${newCourse.name}` });
+        res.status(201).json(newCourse);
     } catch (err) {
         res.status(400).json({ message: err.message });
+    }
+
+});
+
+// Update Course
+router.put('/', async (req, res) => {
+
+    const courseName = req.body.courseName;
+    const newName = req.body.newName;
+    const newItems = req.body.newItems;
+    const newCredits = req.body.newCredits;
+
+    try {
+
+        const courseToUpdate = await Course.findOne({ name: courseName });
+        courseToUpdate.name = newName;
+        courseToUpdate.items = newItems;
+        courseToUpdate.credits = newCredits;
+
+        await courseToUpdate.save();
+        res.status(200).json(courseToUpdate);
+
+    } catch (err) {
+        res.json({ message: err.message });
     }
 
 });
@@ -36,11 +60,11 @@ router.post('/', async (req, res) => {
 // Delete course
 router.delete('/', async (req, res) => {
 
-    const courseName = req.body.name;
-    const courseToDelete = await Course.findOne({ name: courseName });
+    const courseName = req.body.courseName;
 
     try {
 
+        const courseToDelete = await Course.findOne({ name: courseName });
         await courseToDelete.deleteOne();
         res.status(200).json({ message: "Successfully deleted course" });
 
@@ -50,63 +74,98 @@ router.delete('/', async (req, res) => {
 
 });
 
-// Assignments
+// Items
 
-// Get assignments from a course
-// router.get('/allasn', async (req, res) => {
+// Get all assignments from a course
+router.get('/allitems', async (req, res) => {
 
-//     try {
-//         const course = await Course.findOne({ name: req.body.courseName });
-//         res.status(200).json(course.assignments);
+    try {
+        const course = await Course.findOne({ name: req.body.courseName });
+        res.status(200).json(course.items);
 
-//     } catch (err) {
-//         res.status(500).json({ message: err.message });
-//     }
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 
-// });
+});
 
-// router.get('/asn', async (req, res) => {
+// Get an item from a course
+router.get('/item', async (req, res) => {
 
-//     try {
+    try {
 
-//         const assignments = await Item.find();
-//         res.status(200).json(assignments);
+        const course = await Course.findOne({ name: req.body.courseName });
+        const courseItem = course.items.find((item) => item.name === req.body.itemName);
+        res.status(200).json(courseItem);
 
-//     } catch (err) {
-//         res.status(500).json({ message: err.message });
-//     }
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 
-// });
+});
 
-// // Add assignment to course
-// router.post('/asn', async (req, res) => {
+// Add item to course
+router.patch('/item', async (req, res) => {
 
-//     try {
-//         const course = await Course.findOne({ name: req.body.courseName });
-//         const assignment = new Item({
-//             name: req.body.assignmentName,
-//             weight: req.body.weight,
-//             grade: req.body.grade
-//         });
-//         course.assignments.push(assignment);
-//         await course.save();
-//         res.status(201).json(course);
+    const courseName = req.body.courseName;
 
-//     } catch (err) {
-//         res.json({ message: err.message });
-//     }
-// });
+    try {
+        const course = await Course.findOne({ name: courseName });
+        const item = {
+            name: req.body.itemName,
+            weight: req.body.weight,
+            grade: req.body.grade
+        };
+        course.items.push(item);
+        await course.save();
+        res.status(201).json(course);
 
-// // Delete assignment
-// // router.delete('/asn', async (req, res) => {
+    } catch (err) {
+        res.json({ message: err.message });
+    }
+});
 
-// //     try {
-// //         const course = await Course.findOne({ name: req.body.courseName });
+// Edit item
+router.patch('/item/edit', async (req, res) => {
 
-// //     } catch (err) {
+    const courseName = req.body.courseName;
+    const itemName = req.body.itemName;
 
-// //     }
+    try {
 
-// // });
+        const courseToUpdate = await Course.findOne({ name: courseName });
+        const itemToUpdate = courseToUpdate.items.find((item) => item.name === itemName);
+
+        itemToUpdate.name = req.body.name;
+        itemToUpdate.weight = req.body.weight;
+        itemToUpdate.grade = req.body.grade;
+
+        await courseToUpdate.save();
+        res.status(200).json(itemToUpdate);
+        
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+
+});
+
+// // Delete item
+router.delete('/item', async (req, res) => {
+
+    try {
+
+        const course = await Course.findOne({ name: req.body.courseName });
+        const itemToDelete = course.items.find((item) => item.name === req.body.itemName);
+        const itemToDeleteIndex = course.items.indexOf(itemToDelete);
+        course.items.splice(itemToDeleteIndex, 1);
+
+        await course.save();
+        res.status(200).json({ message: "Successfully deleted course" });
+
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+
+});
 
 module.exports = router;
